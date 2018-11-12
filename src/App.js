d7cab51ba700daf1d3c8056f01cc04d27021be6b
Phors <menphorsqty@gmail.com>
2018-11-12 15:14:05 +0700

    Create new parent object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js';
 import person from "./Person/Person";
-class App extends Component {
+//import Property from './General/Property.js';
+//import Property from './General/Property';
+var PropTypes = require('prop-types');
+export default class App extends React.Component {
   state = {
       persons: [
           {id:'abcd1',name : 'Max', age: 26},
@@ -59,13 +61,132 @@ class App extends Component {
       }
       return(
       <div className="App">
-          <img src={logo} className="App-logo" alt="logo" />
           <button style={style}
                   onClick={ ()=>this.togglePersonHander()}
           >Toggle Persons</button>
           {persons}
+          <Parent />
+          <p>prop obj is: ID= {this.props.propObject.obj1.id} Name= {this.props.propObject.obj1.name} Age={this.props.propObject.obj1.age}</p>
+          <p>prop string is: {this.props.propString}</p>
+          <p>prop number is: {this.props.propNumber}</p>
+          <Body />
       </div>);
   }
 }
+class Parent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            cars: ['BMW','TOYOTA','Lexus','Rang Rover']
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.setState({cars: this.state.cars.reverse()});
+    }
+    render(){
+        return(
+            <div>
+                <h1 onClick={this.handleClick}>just info for Parent</h1>
+                <Cars msg="Cars are cool" model="12345" coolCars={this.state.cars}/>
+            </div>
+        );
+    }
 
-export default App;
+}
+Parent.defaultProps = {
+    cars: ['BMW','TOYOTA','Lexus','Rang Rover']
+}
+App.defaultProps = {
+    propObject:{
+        obj1:{
+            id: "1234", name: "Thida", age: "28",
+        },
+        obj2: {
+            id: "12345", name: "ThY", age: "38",
+        }
+
+    },
+    propString: "This is prop string",
+    propNumber:3,
+}
+
+App.propTypes = {
+    propObject: PropTypes.object.isRequired,
+    propString: PropTypes.string,
+    propNumber: PropTypes.number
+}
+class Cars extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h3>just info for cars</h3>
+                <p>{this.props.msg}</p>
+                <p>{this.props.model}</p>
+                <div>{this.props.coolCars.map((item,i) => {
+                    return <p key={i}>{item}</p>;
+                    }
+                )}</div>
+            </div>
+        );
+    }
+}
+class Body extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            r:0
+        };
+        this.getRandomNumer = this.getRandomNumer.bind(this)
+    }
+    getRandomNumer(){
+        //console.log("Random number called");
+        this.setState({r: Math.floor(Math.random()* 10) })
+    }
+    render(){
+        return (
+          <div>
+              <p className="App-intro">
+                  <button onClick={this.getRandomNumer}> Random Number</button>
+              </p>
+              <Numbers myNumber={this.state.r} />
+          </div>
+
+        );
+    }
+}
+class Numbers extends Component {
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+    componentWillReceiveProps(newProps){
+        console.log('recieve');
+    }
+    shouldComponentUpdate(newProps, nextState){
+        console.log('unmount');
+        return true;
+    }
+    componentWillUpdate(newProps, nextState){
+        console.log('componentWillUpdate');
+    }
+    componentDidUpdate(newProps, nextState){
+        console.log('componentDidUpdate');
+    }
+    componentWillUnmount(){
+        console.log('unmount');
+    }
+    render(){
+        return (
+          <div>
+              {this.props.myNumber}
+          </div>
+        );
+    }
+}
+//export default App;
---
+++ src/General/Property.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Property.css';
+const cars = <div>
+    <h1>ABC</h1>
+</div>;
+
+
